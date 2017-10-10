c30b36e38e229897624a00c121c851f090aa2ba2
Tegwen Gabb <fj803263@live.reading.ac.uk>
2017-10-10 14:36:59 +0100

    Changed import from future as was incorrect
    Created a function to make an array for yi

--- week3 assisnment Q1.py
+++ week3 assisnment Q1.py
@@ -7,7 +7,16 @@ Created on Tue Oct 10 13:34:53 2017
 
 #Question one of Numerical Differentiation assignment
 
-from ___future___ import division
+from __future__ import division
 import numpy as np
 
+def genarray(y0,yN,N):
+    #function to generate an array of y's
+    n=int(N)
+    dy=( float(yN) - float(y0) ) / float(n + 1)
+    yarray=np.zeros(n)
+    for i in xrange(n):
+        yarray[i] = y0 + i * dy
+    return yarray
 
+print genarray(0,1e6,10)
